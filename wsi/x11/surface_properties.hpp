611d3b51601842377f4f4cb982b516a5b0f75b2b
Ivan Podogov <ginkage.uk@gmail.com>
2026-07-01 22:18:05 +0100

    wsi/x11: support VK_PRESENT_MODE_IMMEDIATE_KHR
    Advertise IMMEDIATE as a third X11 present mode. In the DRI3 presenter
    it ORs XCB_PRESENT_OPTION_ASYNC into the present options so the X server
    presents as soon as possible without waiting for vblank (tearing
    permitted); like MAILBOX it skips the FIFO presentation thread and
    derives unpaced recycling from the present mode. Reachable when the
    application requests IMMEDIATE and WSI_ALLOW_NON_FIFO_PRESENT_MODE is
    set (the layer otherwise forces FIFO).
    
    Adapted from the XCB present path in cixtech/vulkan-wsi-layer commit
    33eafb6 ("CIX: Add XCB/Xlib WSI support"), which uses
    XCB_PRESENT_OPTION_ASYNC for IMMEDIATE.
    
    Signed-off-by: Ivan Podogov <ginkage.uk@gmail.com>

--- wsi/x11/surface_properties.hpp
+++ wsi/x11/surface_properties.hpp
@@ -74,10 +74,10 @@ private:
    surface *specific_surface;
 
    /* List of supported presentation modes */
-   std::array<VkPresentModeKHR, 2> m_supported_modes;
+   std::array<VkPresentModeKHR, 3> m_supported_modes;
 
    /* Stores compatible presentation modes */
-   compatible_present_modes<2> m_compatible_present_modes;
+   compatible_present_modes<3> m_compatible_present_modes;
 
    void populate_present_mode_compatibilities() override;
 
