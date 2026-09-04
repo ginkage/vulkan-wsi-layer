0c1df1b64f939dec4ab5b510a3f59309e45408f2
Alex Bates <alex.bates@arm.com>
2026-09-04 11:56:11 +0000

    Enforce newline at end of file
    Adds `-Wnewline-eof` to the clang-tidy pipeline to prevent source code 
    with no newline at EOF from being merged. Also fixes all existing 
    files lacking newline at EOF.
    
    Signed-off-by: Alex Bates <alex.bates@arm.com>
    Change-Id: I79b3307cfe05834fad275590e5ab4b4e1825d5e0

--- util/debug.hpp
+++ util/debug.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Arm Limited.
+ * Copyright (c) 2025-2026 Arm Limited.
  *
  * SPDX-License-Identifier: MIT
  *
--- util/wsialloc/wsialloc_helpers.h
+++ util/wsialloc/wsialloc_helpers.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Arm Limited.
+ * Copyright (c) 2024, 2026 Arm Limited.
  *
  * SPDX-License-Identifier: MIT
  *
